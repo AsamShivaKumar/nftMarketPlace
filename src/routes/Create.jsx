2ce21b6ac00458ceea601cea3600c852c998c0f8
AsamShivaKumar <asamshivakumar@gmail.com>
2022-07-16 21:30:40 +0530

    Modified Create.jsx

--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import {useCookies} from 'react-cookie';
 import {useNavigate} from 'react-router-dom';
+import "../styles/create.css";
 import Navbar from "../components/Navbar.jsx";
+
 
 function Create(){
     const [cookies] = useCookies();
@@ -13,6 +15,20 @@ function Create(){
     return (
       <>
         <Navbar current="create" />
+        <div className="formDiv">
+          <div className="textDiv">
+             <h1 className="createText">Create your own NFT</h1>
+             <p>Specify the name, give the description for the NFT and upload the file.</p>
+             <p>That's it! You own an NFT now!</p>
+             <div className="divBg"></div>
+          </div>
+          <form>
+            <input type="text"></input>
+            <textarea>...Description</textarea>
+            <input type="file"></input>
+            <button type="submit">Create NFT</button>
+          </form>
+        </div>
       </>
     )
 }
